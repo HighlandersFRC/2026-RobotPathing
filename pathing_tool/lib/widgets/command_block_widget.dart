b1119ca5864832f95583371902b22ea7f820de6a
Advik-highlanders28 <144296737+Advik-highlanders28@users.noreply.github.com>
2025-12-30 15:48:06 -0700

    imports

--- pathing_tool/lib/widgets/command_block_widget.dart
+++ pathing_tool/lib/widgets/command_block_widget.dart
@@ -118,9 +118,9 @@
 //   }
 // }
 import 'package:flutter/material.dart';
-import '../models/command_block.dart';
-import '../services/geometry_utils.dart';
-import '../ui/styles.dart';
+import 'package:pathing_tool/models/command_block.dart';
+import 'package:pathing_tool/services/geometry_utils.dart';
+import 'package:pathing_tool/ui/styles.dart';
 
 class CommandBlockWidget extends StatelessWidget {
   final CommandBlock commandBlock;
